7047d54068fe7a392114899e843a756071bd12fd
Hajime Arends <harends97@gmail.com>
2023-09-01 20:22:48 -0700

    configure server.js

--- public/server.js
+++ public/server.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const path = require('path');
-const fileupload = require('express-fileupload')
+const fileupload = require('express-fileupload');
+
+let initial_path = path.join(__dirname, "public");
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, "home.html"));
+})
+
+app.listen("3000", () => {
+    console.log('listening......');
+})
