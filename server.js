fe95b972568c0295124940d98a0fffcd0bb2cea0
Hajime Arends <harends97@gmail.com>
2023-09-01 20:46:14 -0700

    configure server, add navbar

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const fileupload = require('express-fileupload');
 
 let initial_path = path.join(__dirname, "public");
 
+const app = express();
+app.use(express.static(initial_path));
+app.use(fileupload());
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(initial_path, "home.html"));
 })
